777e34a99b9cd3d4bbc6c264923552f60656594f
affsvoyo <githubunoficial@gmail.com>
2026-04-02 17:44:13 -0300

    Update Main.hx

--- source/funkin/backend/system/Main.hx
+++ source/funkin/backend/system/Main.hx
@@ -151,7 +151,7 @@ class Main extends Sprite
 		if (FileSystem.exists("mods/autoload.txt"))
 			modToLoad = File.getContent("mods/autoload.txt").trim();
 
-		ModsFolder.switchMod(modToLoad.getDefault(D&B-Unfair));
+		ModsFolder.switchMod(D&B-Unfair);
 		#end
 
 		initTransition();
